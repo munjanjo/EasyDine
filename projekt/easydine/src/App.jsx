b180b223bb9b83924729f709cb35b6e99079034c
munjanjo <147388581+munjanjo@users.noreply.github.com>
2024-10-22 17:15:50 +0200

    Add files via upload

---
+++ projekt/easydine/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Button from "./button";
+function App() {
+  return(
+  <>
+    
+    <Header/>
+    <Button/>
+    
+  </>
+    
+  );
+}
+
+export default App
